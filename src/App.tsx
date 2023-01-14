7eddd25dd6ceb92932a7beb55bf40c5f80ccb873
Eugeniya Volegzhanina <92241355+VolegzhaninaEM@users.noreply.github.com>
2023-01-14 21:39:35 +0700

    Revert "React example2"

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import {Product} from './components/Product';
-import {useProducts} from "./hooks/products";
-import {Loader} from "./components/Loader";
-import {ErrorMessage} from "./components/ErrorMessage";
+import React,
+{
+    createElement as el,
+    useState
+}
+from 'react';
 
 function App() {
-    const {loading, products, error} = useProducts();
-
-    return (
-        <div className="container mx-auto max-w-2xl pt-5">
-            { loading && <Loader /> }
-            { error &&  <ErrorMessage error={error}/>}
-            { products.map(product => <Product product={product} key={product.id}/>) }
-
-        </div>
-    );
+    const [count, setCount] = useState(0)
+  return (
+      el('div', {className: 'antialiased font-sans bg-gray-200 overflow-hidden bg-gray-100' +
+              'mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'}, [
+          el('h1', {className: 'text-4xl font-extrabold tracking-tight text-slate-900', key: 1},
+              `Hello TSX ${count}`),
+          el('button', {
+              className: 'bg-red-200 pointer-events-auto rounded-md py-2 px-4 ' +
+                  'text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50' +
+                  'relative flex h-8 items-center justify-between',
+              key: 2,
+              onClick: () => {
+                  setCount(count +1)
+              }}, 'Click me!!')
+      ])
+  );
 }
 
 export default App;
