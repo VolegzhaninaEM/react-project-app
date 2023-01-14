0472a67a9382e4d69a8607a03a0b2e722f7c667c
Jane <velick@list.ru>
2023-01-14 21:04:08 +0700

    Вывела продукты в шаблон

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React,
 {
-    useEffect
+    useEffect, useState
 }
     from 'react';
 import axios from "axios";
 import {Product} from './components/Product';
-import {products} from "./data/products";
+// import {products} from "./data/products";
 import {IProduct} from "./models";
 
 function App() {
+    const [products, setProducts] = useState<IProduct[]>([]);
 
     async function fetchProduct() {
         const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-        console.log(response);
+        setProducts(response.data);
     }
 
     useEffect(() => {
