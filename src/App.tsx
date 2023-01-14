b41242f16af10c5c1b94e9eb5cffdbaf418edbca
Jane <velick@list.ru>
2023-01-14 20:35:35 +0700

    Добавила циклы, есть несколько продуктов, работает кнопка с описанием

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import React,
 }
 from 'react';
 import {Product} from './components/Product';
+import {products} from "./data/products";
 
 function App() {
   return (
       <div className="container mx-auto max-w-2xl pt-5">
-          <Product />
+
+          { products.map(product => <Product product={product} key={product.id}/>) }
+
       </div>
   );
 }
