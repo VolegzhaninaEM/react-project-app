ed7a06a3264577d4343b5b03c06c5c2c2ec47ed4
Jane <velick@list.ru>
2023-01-14 19:39:36 +0700

    Добавила компонент

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,13 @@ import React,
     useState
 }
 from 'react';
+import {Product} from './components/Product';
 
 function App() {
-    const [count, setCount] = useState(0)
   return (
-      el('div', {className: 'antialiased font-sans bg-gray-200 overflow-hidden bg-gray-100' +
-              'mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'}, [
-          el('h1', {className: 'text-4xl font-extrabold tracking-tight text-slate-900', key: 1},
-              `Hello TSX ${count}`),
-          el('button', {
-              className: 'bg-red-200 pointer-events-auto rounded-md py-2 px-4 ' +
-                  'text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50' +
-                  'relative flex h-8 items-center justify-between',
-              key: 2,
-              onClick: () => {
-                  setCount(count +1)
-              }}, 'Click me!!')
-      ])
+      <div className="container mx-auto max-w-2xl pt-5">
+          <Product />
+      </div>
   );
 }
 
