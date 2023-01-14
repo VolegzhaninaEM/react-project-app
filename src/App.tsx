3f47175f657024ddfbd328eae4dc6a6030fc6a20
Jane <velick@list.ru>
2023-01-14 20:57:08 +0700

    Добавила продукты из http://fakestoreapi.com/docs через асинхронную функцию

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import React,
 {
-    createElement as el,
-    useState
+    useEffect
 }
-from 'react';
+    from 'react';
+import axios from "axios";
 import {Product} from './components/Product';
 import {products} from "./data/products";
+import {IProduct} from "./models";
 
 function App() {
-  return (
-      <div className="container mx-auto max-w-2xl pt-5">
 
-          { products.map(product => <Product product={product} key={product.id}/>) }
+    async function fetchProduct() {
+        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+        console.log(response);
+    }
 
-      </div>
-  );
+    useEffect(() => {
+        fetchProduct();
+    }, []);
+
+    return (
+        <div className="container mx-auto max-w-2xl pt-5">
+
+            {products.map(product => <Product product={product} key={product.id}/>)}
+
+        </div>
+    );
 }
 
 export default App;
